78895833fc06439f518b87cffa73facf48c352f6
gilheec <gilheec@naver.com>
2017-11-04 15:53:49 +0900

    통신관련 로직 추가

--- app/src/main/java/org/gilheec/smartalarm/BrlistActivity.java
+++ app/src/main/java/org/gilheec/smartalarm/BrlistActivity.java
@@ -1,12 +1,15 @@
 package org.gilheec.smartalarm;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,12 +17,22 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BrlistActivity extends AppCompatActivity {
     // ListView listView = null;
+
+    private static final String BRLIST_URL = "http://172.16.1.252:3000/branch/list";
 
     class Item {
         String branch_name;
@@ -46,7 +59,6 @@ public class BrlistActivity extends AppCompatActivity {
         public String getBranch_no() {
             return branch_no;
         }
-
     }
 
     ArrayList<Item> itemList = new ArrayList<Item>();
@@ -89,15 +101,19 @@ public class BrlistActivity extends AppCompatActivity {
 
         ListView listView = (ListView)findViewById(R.id.listview);
 
+        /* 통신로직 쪽으로 이동        */
+        /*
         itemList.add(new Item("광교영업부", "서울 중구 청계천로 54", "12m", "3001"));
         itemList.add(new Item("서울롯데", "서울특별시 중구 을지로 30, (롯데호텔)", "272m", "3002"));
         itemList.add(new Item("종각역", "서울특별시 종로구 종로 33", "301m", "3003"));
         itemList.add(new Item("파이낸스센터", "서울특별시 중구 세종대로 136, (파이낸스빌딩)", "312m", "3004"));
         itemList.add(new Item("광화문", "서울특별시 중구 세종대로 135-5", "452m", "3005"));
+        */
 
         itemAdpater = new ItemAdapter(BrlistActivity.this, R.layout.list_item, itemList);
         listView.setAdapter(itemAdpater);
 
+        Log.d("debug", "aaaaaaaaaaaaaaaaa");
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -114,5 +130,78 @@ public class BrlistActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        // 통신하여 영업점 리스트를 가지고 온다.
+        new ConnBrlist().execute(BRLIST_URL);
     }
+
+    // ConnBrlist  에 대한 Async 통신 처리
+    class ConnBrlist extends AsyncTask<String,String,String> {
+        ProgressDialog dialog = new ProgressDialog(BrlistActivity.this);
+        @Override
+        protected String doInBackground(String... params) {
+            StringBuilder output = new StringBuilder();
+            try {
+                URL url = new URL(params[0]);
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                if (conn != null) {
+                    conn.setConnectTimeout(10000);
+                    conn.setRequestMethod("GET");
+                    //conn.setDoInput(true); conn.setDoOutput(true);
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(conn.getInputStream()));
+                    String line = null;
+                    while(true) {
+                        line = reader.readLine();
+                        if (line == null) break;
+                        output.append(line);
+                    }
+                    reader.close();
+                    conn.disconnect();
+                }
+            } catch (Exception e) { e.printStackTrace(); }
+            return output.toString();
+        }
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog.setMessage("가까운 영업점 확인중..");
+            dialog.show();
+        }
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            dialog.dismiss();
+            try {
+                JSONObject json = new JSONObject(s);
+
+                if (json.getBoolean("result") == true) {//통신 성공
+
+                    Log.d("debug", "bbbbbb");
+                    JSONArray ny_array = json.getJSONArray("ny");
+
+                    for (int ix = 0; ix < ny_array.length(); ix++)
+                    {
+                        JSONObject obj = ny_array.getJSONObject(ix);
+
+                        String branch_name = obj.getString("branch_name");
+                        String address = obj.getString("address");
+                        String position = obj.getString("position_x") + "m";
+                        String branch_no = obj.getString("branch_no");
+
+                        itemList.add(new Item(branch_name, address, position, branch_no));
+                    }
+                    itemAdpater.notifyDataSetChanged();
+
+                } else {//통신 실패
+
+                    Log.d("debug", "ccccccc");
+                    Toast.makeText(BrlistActivity.this,
+                            json.getString("err"),
+                            Toast.LENGTH_SHORT).show();
+                }
+            } catch (Exception e) { e.printStackTrace(); }
+        }
+    }
+
 }
--- app/src/main/java/org/gilheec/smartalarm/EnterActivity.java
+++ app/src/main/java/org/gilheec/smartalarm/EnterActivity.java
@@ -1,12 +1,32 @@
 package org.gilheec.smartalarm;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Iterator;
 
 public class EnterActivity extends AppCompatActivity {
+
+    private static final String ENTER_URL = "http://172.16.1.252:3000/orderno";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +37,13 @@ public class EnterActivity extends AppCompatActivity {
     {
         Intent intent = new Intent(EnterActivity.this, BrlistActivity.class);
         startActivityForResult(intent, 1);
+    }
+    public void onSerno(View view)
+    {
+        // 통신하여 영업점 리스트를 가지고 온다.
+        String regId = FirebaseInstanceId.getInstance().getToken();
+        Log.i("debug ", "device create " + regId);
+        new ConnEnter().execute(ENTER_URL, regId);
     }
 
     @Override
@@ -39,5 +66,109 @@ public class EnterActivity extends AppCompatActivity {
                 break;
         }
         //super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    // ConnEnter  에 대한 Async 통신 처리
+    class ConnEnter extends AsyncTask<String,String,String> {
+        ProgressDialog dialog = new ProgressDialog(EnterActivity.this);
+        TextView branchNoText = (TextView)findViewById(R.id.branch_no);
+        TextView orderNoText = (TextView)findViewById(R.id.orderno);
+
+        @Override
+        protected String doInBackground(String... params) {
+            StringBuilder output = new StringBuilder();
+            try {
+                URL url = new URL(params[0]);
+                JSONObject postDataParams = new JSONObject();
+                postDataParams.put("device_token", params[1]);
+                postDataParams.put("branch_no", branchNoText.getText());
+                postDataParams.put("order_no", orderNoText.getText());
+
+                /// 입력값 추가 postDataParams....
+
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                if (conn != null) {
+                    conn.setConnectTimeout(10000);
+                    conn.setRequestMethod("POST");
+                    conn.setDoInput(true); conn.setDoOutput(true);
+                    OutputStream os = conn.getOutputStream();
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+                    writer.write(getPostDataString(postDataParams));
+                    writer.flush();
+                    writer.close();
+                    os.close();
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(conn.getInputStream()));
+                    String line = null;
+                    while(true) {
+                        line = reader.readLine();
+                        if (line == null) break;
+                        output.append(line);
+                    }
+                    reader.close();
+                    conn.disconnect();
+                }
+            } catch (Exception e) { e.printStackTrace(); }
+            return output.toString();
+        }
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog.setMessage("순번 등록 처리중..");
+            dialog.show();
+        }
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            dialog.dismiss();
+            try {
+                JSONObject json = new JSONObject(s);
+
+                Log.d("debug", json.toString());
+
+                Log.d("debug", "ccccccccccccccccccccc");
+
+                if (json.getBoolean("result") == true) { //통신 성공
+
+                    Log.d("debug", "eeeeeeeeeeeeeeeeeeeee");
+
+                    Intent intent = new Intent(EnterActivity.this, StatusActivity.class);
+                    startActivityForResult(intent, 1);
+                    finish();
+
+                } else { //통신 실패
+
+                    Log.d("debug", "fffffffffffffffffffffff");
+
+                    Toast.makeText(EnterActivity.this,
+                            json.getString("err"),
+                            Toast.LENGTH_SHORT).show();
+                }
+            } catch (Exception e) { e.printStackTrace(); }
+        }
+    }
+
+    public String getPostDataString(JSONObject params) throws Exception {
+
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+
+        Iterator<String> itr = params.keys();
+
+        while(itr.hasNext()){
+
+            String key= itr.next();
+            Object value = params.get(key);
+
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(key, "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(value.toString(), "UTF-8"));
+        }
+        return result.toString();
     }
 }
