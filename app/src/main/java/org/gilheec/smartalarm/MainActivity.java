a85744ea9bc37984380faeb380f5b5a7e027c553
gilheec <gilheec@naver.com>
2017-11-10 17:39:22 +0900

    상세기능 구현하기

--- app/src/main/java/org/gilheec/smartalarm/MainActivity.java
+++ app/src/main/java/org/gilheec/smartalarm/MainActivity.java
@@ -1,17 +1,149 @@
 package org.gilheec.smartalarm;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String CHECK_URL = "http://172.16.1.252:3000/orderno/check";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("debug", "yyyyyyyyyyyyyyyyyyyyyyyy");
+
+        // 통신하여 대기상황을 가지고 온다
+        String regId = FirebaseInstanceId.getInstance().getToken();
+        Log.i("debug", "device create " + regId);
+        new ConnCheck().execute(CHECK_URL, regId);
     }
+
+    // ConnStatus  에 대한 Async 통신 처리
+    class ConnCheck extends AsyncTask<String,String,String> {
+        ProgressDialog dialog = new ProgressDialog(MainActivity.this);
+
+        @Override
+        protected String doInBackground(String... params) {
+            StringBuilder output = new StringBuilder();
+            try {
+                URL url = new URL(params[0]);
+                JSONObject postDataParams = new JSONObject();
+                postDataParams.put("device_token", params[1]);
+
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                if (conn != null) {
+                    conn.setConnectTimeout(10000);
+                    conn.setRequestMethod("POST");
+                    conn.setDoInput(true); conn.setDoOutput(true);
+                    OutputStream os = conn.getOutputStream();
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+                    writer.write(getPostDataString(postDataParams));
+                    writer.flush();
+                    writer.close();
+                    os.close();
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(conn.getInputStream()));
+                    String line = null;
+                    while(true) {
+                        line = reader.readLine();
+                        if (line == null) break;
+                        output.append(line);
+                    }
+                    reader.close();
+                    conn.disconnect();
+                }
+            } catch (Exception e) { e.printStackTrace(); }
+            return output.toString();
+        }
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog.setMessage("대기순번 등록여부 확인중..");
+            dialog.show();
+        }
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            dialog.dismiss();
+            try {
+                JSONObject json = new JSONObject(s);
+
+                Log.d("debug", json.toString());
+
+                Log.d("debug", "ccccccccccccccccccccc");
+
+                if (json.getBoolean("result") == true) { //통신 성공
+
+                    Log.d("debug", "bbbbbb");
+
+                    int cnt = json.getInt("cnt");
+
+                    if (cnt > 0) {
+                        Intent intent = new Intent(MainActivity.this, StatusActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Intent intent = new Intent(MainActivity.this, EnterActivity.class);
+                        startActivity(intent);
+                    }
+
+                } else { //통신 실패
+
+                    Log.d("debug", "fffffffffffffffffffffff");
+
+                    Toast.makeText(MainActivity.this,
+                            json.getString("err"),
+                            Toast.LENGTH_SHORT).show();
+                }
+            } catch (Exception e) { e.printStackTrace(); }
+        }
+    }
+
+    public String getPostDataString(JSONObject params) throws Exception {
+
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+
+        Iterator<String> itr = params.keys();
+
+        while(itr.hasNext()){
+
+            String key= itr.next();
+            Object value = params.get(key);
+
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(key, "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(value.toString(), "UTF-8"));
+        }
+        return result.toString();
+    }
+
     public void onEnterScr(View view)
     {
         Intent intent = new Intent(MainActivity.this, EnterActivity.class);
--- app/src/main/java/org/gilheec/smartalarm/StatusActivity.java
+++ app/src/main/java/org/gilheec/smartalarm/StatusActivity.java
@@ -1,11 +1,15 @@
 package org.gilheec.smartalarm;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,13 +17,32 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.iid.FirebaseInstanceId;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import static org.gilheec.smartalarm.R.id.status_ctnt;
 
 public class StatusActivity extends AppCompatActivity {
+
+    private static final String STATUS_URL = "http://172.16.1.252:3000/orderno/status";
+    private static final String CANCEL_URL = "http://172.16.1.252:3000/orderno";
+
     class Item {
         String status_ctnt;
 
@@ -44,7 +67,12 @@ public class StatusActivity extends AppCompatActivity {
 
             TextView text1View = (TextView)convertView.findViewById(status_ctnt);
             Item item = itemList.get(position);
-            text1View.setText(item.status_ctnt);
+            if (position == itemList.size()-1) {
+                text1View.setText(Html.fromHtml("<font color='red'><b>"+item.status_ctnt+"</b></font>"));
+            } else {
+                text1View.setText(item.status_ctnt);
+            }
+
 
             return convertView;
         }
@@ -63,14 +91,19 @@ public class StatusActivity extends AppCompatActivity {
 
         ListView listView = (ListView)findViewById(R.id.listview);
 
+        /* 통신로직 쪽으로 이동        */
+        /*
         itemList.add(new Item("0012 - 호출 (대기시간 25분)"));
         itemList.add(new Item("0013 - 호출 (대기시간 24분)"));
         itemList.add(new Item("0014 - 대기 (대기시간 23분)"));
         itemList.add(new Item("0015 - 대기 (대기시간 20분)"));
         itemList.add(new Item("0016 - 대기 (대기시간 18분)"));
+        */
 
         itemAdpater = new ItemAdapter(StatusActivity.this, R.layout.list_item, itemList);
         listView.setAdapter(itemAdpater);
+
+        Log.d("debug", "uuuuuuuuuuuuuuuu");
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -81,13 +114,230 @@ public class StatusActivity extends AppCompatActivity {
                 // item.getStatus_ctnt() 사용...
             }
         });
+
+        Log.d("debug", "yyyyyyyyyyyyyyyyyyyyyyyy");
+
+        // 통신하여 대기상황을 가지고 온다
+        String regId = FirebaseInstanceId.getInstance().getToken();
+        Log.i("debug", "device create " + regId);
+        new ConnStatus().execute(STATUS_URL, regId);
+    }
+
+    // ConnStatus  에 대한 Async 통신 처리
+    class ConnStatus extends AsyncTask<String,String,String> {
+        ProgressDialog dialog = new ProgressDialog(StatusActivity.this);
+
+        @Override
+        protected String doInBackground(String... params) {
+            StringBuilder output = new StringBuilder();
+            try {
+                URL url = new URL(params[0]);
+                JSONObject postDataParams = new JSONObject();
+                postDataParams.put("device_token", params[1]);
+
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                if (conn != null) {
+                    conn.setConnectTimeout(10000);
+                    conn.setRequestMethod("POST");
+                    conn.setDoInput(true); conn.setDoOutput(true);
+                    OutputStream os = conn.getOutputStream();
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+                    writer.write(getPostDataString(postDataParams));
+                    writer.flush();
+                    writer.close();
+                    os.close();
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(conn.getInputStream()));
+                    String line = null;
+                    while(true) {
+                        line = reader.readLine();
+                        if (line == null) break;
+                        output.append(line);
+                    }
+                    reader.close();
+                    conn.disconnect();
+                }
+            } catch (Exception e) { e.printStackTrace(); }
+            return output.toString();
+        }
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog.setMessage("순번 대기상황 조회중..");
+            dialog.show();
+        }
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            dialog.dismiss();
+            try {
+                JSONObject json = new JSONObject(s);
+
+                Log.d("debug", json.toString());
+
+                Log.d("debug", "ccccccccccccccccccccc");
+
+                if (json.getBoolean("result") == true) { //통신 성공
+
+
+                    Log.d("debug", "bbbbbb");
+
+                    JSONArray ny_array = json.getJSONArray("ny");
+
+                    itemList.clear(); //chon
+                    for (int ix = 0; ix < ny_array.length(); ix++)
+                    {
+                        JSONObject obj = ny_array.getJSONObject(ix);
+
+                        Log.d("debug", "iiiiiiiiiiiiiiiiiiiiiiiiiiiii");
+
+                        int order_no = obj.getInt("order_no");
+                        int wait_status_cd = obj.getInt("wait_status_cd");
+                        String dr_time = obj.getString("dr_time");
+                        String wait_mm = obj.getString("wait_mm");
+                        String call_time = obj.getString("call_time");
+
+                        String status_ctnt = "";
+
+                        Log.d("debug", "iiiiiiiiiiiiiiiiiiiiiiiiiiiii" + order_no + "|" + wait_status_cd + "|" + dr_time + "|" + wait_mm);
+
+                        if (wait_status_cd == 1) {
+                            // "0013 - 대기중 (대기시간 25분)";
+                            status_ctnt = order_no + " -  대기중 (대기시간 " + wait_mm + "분)";
+                        } else {
+                            // "0012 - 호출   (호출시간 00:00:00)";
+                            status_ctnt = order_no + " -  호출 (호출시간 " + call_time + ")";
+                        }
+
+                        itemList.add(new Item(status_ctnt));
+                    }
+
+                    itemAdpater.notifyDataSetChanged();
+
+                } else { //통신 실패
+
+                    Log.d("debug", "fffffffffffffffffffffff");
+
+                    Toast.makeText(StatusActivity.this,
+                            json.getString("err"),
+                            Toast.LENGTH_SHORT).show();
+                }
+            } catch (Exception e) { e.printStackTrace(); }
+        }
+    }
+
+    public String getPostDataString(JSONObject params) throws Exception {
+
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+
+        Iterator<String> itr = params.keys();
+
+        while(itr.hasNext()){
+
+            String key= itr.next();
+            Object value = params.get(key);
+
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(key, "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(value.toString(), "UTF-8"));
+        }
+        return result.toString();
+    }
+
+    public void onBtnReflesh(View view) {
+        Log.d("debug", "eeeeeeeeeeeeeeeeeeeeeeee");
+
+        // 통신하여 대기상황을 가지고 온다
+        String regId = FirebaseInstanceId.getInstance().getToken();
+        Log.i("debug", "device create " + regId);
+        new ConnStatus().execute(STATUS_URL, regId);
     }
 
     public void onBtnCancel(View view) {
-        ;
+        Log.d("debug", "eeeeeeeeeeeeeeeeeeeeeeee");
+
+        // 취소처리한다
+        String regId = FirebaseInstanceId.getInstance().getToken();
+        Log.i("debug", "device create " + regId);
+        new ConnCancel().execute(CANCEL_URL, regId);
     }
 
-    public void onBtnCall(View view) {
-        ;
+    // ConnEnter  에 대한 Async 통신 처리
+    class ConnCancel extends AsyncTask<String,String,String> {
+        ProgressDialog dialog = new ProgressDialog(StatusActivity.this);
+
+        @Override
+        protected String doInBackground(String... params) {
+            StringBuilder output = new StringBuilder();
+            try {
+                URL url = new URL(params[0]);
+                JSONObject postDataParams = new JSONObject();
+                postDataParams.put("device_token", params[1]);
+
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                if (conn != null) {
+                    conn.setConnectTimeout(10000);
+                    conn.setRequestMethod("DELETE");
+                    conn.setDoInput(true); conn.setDoOutput(true);
+                    OutputStream os = conn.getOutputStream();
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+                    writer.write(getPostDataString(postDataParams));
+                    writer.flush();
+                    writer.close();
+                    os.close();
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(conn.getInputStream()));
+                    String line = null;
+                    while(true) {
+                        line = reader.readLine();
+                        if (line == null) break;
+                        output.append(line);
+                    }
+                    reader.close();
+                    conn.disconnect();
+                }
+            } catch (Exception e) { e.printStackTrace(); }
+            return output.toString();
+        }
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog.setMessage("순번 대기 취소처리중..");
+            dialog.show();
+        }
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            dialog.dismiss();
+            try {
+                JSONObject json = new JSONObject(s);
+
+                Log.d("debug", json.toString());
+
+                Log.d("debug", "ccccccccccccccccccccc");
+
+                if (json.getBoolean("result") == true) { //통신 성공
+
+                    Log.d("debug", "eeeeeeeeeeeeeeeeeeeee");
+                    finish();
+
+                } else { //통신 실패
+
+                    Log.d("debug", "fffffffffffffffffffffff");
+
+                    Toast.makeText(StatusActivity.this,
+                            json.getString("err"),
+                            Toast.LENGTH_SHORT).show();
+                }
+            } catch (Exception e) { e.printStackTrace(); }
+        }
     }
+
+
 }
